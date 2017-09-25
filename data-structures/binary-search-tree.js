23347ac00791092c6004ba0498dee3f1e8df951a
Aj Leino <anthony.leinojr@gmail.com>
2017-09-24 22:59:36 -0500

    Added/refactored insertion code for BST, as well as additional testing for element insertion. Added methods for traversal through the tree. Added method for returning contents of tree in an ordered array.

--- data-structures/binary-search-tree.js
+++ data-structures/binary-search-tree.js
@@ -3,23 +3,26 @@
   */
 
 /**
- * Class constructor for a node within the binary search tree. Node objects
- * contain a key (number), as well as possibly a linked left or right node.
- * @param       {number} key [description]
- * @constructor
- */
-function Node(key) {
-    this.leftNode = null;
-    this.rightNode = null;
-    this.key = key;
-}
-
-/**
  * Class constructor for a basic binary search tree that stores numbers.
  * @constructor
  */
 function BinarySearchTree() {
     this.root = null;
+}
+
+/**
+* Class constructor for a node within the binary search tree. Node objects
+* contain a key (number), as well as possibly a linked left or right node.
+*
+* This definition is effectively a static definition on the class.
+*
+* @param       {number} key The number that this node will hold.
+* @constructor
+*/
+BinarySearchTree.Node = function (key) {
+    this.left = null;
+    this.right = null;
+    this.key = key;
 }
 
 /**
@@ -35,9 +38,9 @@ BinarySearchTree.prototype.contains = function (key) {
         if (key === currNode.key) {
             return true;
         } else if (key < currNode.key) {
-            currNode = currNode.leftNode;
+            currNode = currNode.left;
         } else {
-            currNode = currNode.rightNode;
+            currNode = currNode.right;
         }
     }
     return false;
@@ -50,19 +53,96 @@ BinarySearchTree.prototype.contains = function (key) {
  * @return {boolean}    True if value is contained within the BST; otherwise, false.
  */
 BinarySearchTree.prototype.containsRecursive = function (key) {
-    function containsHelper (key, node) {
+    function containsHelper (node, key) {
         if (node === null) {
             return false;
         } else if (node.key === key) {
             return true;
         }
         if (key < node.key) {
-            return containsHelper(key, node.left);
+            return containsHelper(node.left, key);
         }
-        return containsHelper(key, node.right);
+        return containsHelper(node.right, key);
     }
 
-    return containsHelper(key, this.root);
+    return containsHelper(this.root, key);
+};
+
+/**
+ * Inserts a number into the binary search tree, if it is unique.
+ *
+ * @param  {number} key The number to insert into the tree.
+ */
+BinarySearchTree.prototype.insert = function (key) {
+    this.root = this._insertHelper(this.root, key);
+};
+
+/**
+ * Converts the BST into an ordered array in ascending value.
+ *
+ * @return {Array} The array of ordered values within the tree.
+ */
+BinarySearchTree.prototype.toInOrderArray = function () {
+    var arr = [];
+
+    this._inOrder(this.root, function (currNode) {
+        arr.push(currNode.key);
+    });
+
+    return arr;
+};
+
+/**
+ * Helper method for <code>insert</code>. Provided key is inserted into the
+ * BST, creating a new leaf if the key is unique.
+ *
+ * @param  {Node} node   The current node to process for key insertion.
+ * @param  {number} key  The number to add to the current subtree.
+ * @return {Node}        The current or newly created node.
+ */
+BinarySearchTree.prototype._insertHelper = function (node, key) {
+    if (node === null) {
+        return new BinarySearchTree.Node(key);
+    }
+
+    if (key < node.key) {
+        node.left = this._insertHelper(node.left, key);
+    } else if (key > node.key) {
+        node.right = this._insertHelper(node.right, key);
+    }
+
+    return node;
+};
+
+/**
+ * Traverses through the BST in order (left-to-right increasing), calling the
+ * provided the callback function on each node.
+ *
+ * @param  {Node}     node     The current node being traversed through.
+ * @param  {Function} callback The function to call on the current node.
+ */
+BinarySearchTree.prototype._inOrder = function (node, callback) {
+    if (node !== null) {
+        this._inOrder(node.left, callback);
+        callback(node);
+        this._inOrder(node.right, callback);
+    }
+};
+
+BinarySearchTree.prototype._preOrder = function (node, callback) {
+    if (node !== null) {
+        callback(node);
+        this._preOrder(node.left, callback);
+        this._preOrder(node.right, callback);
+    }
+};
+
+BinarySearchTree.prototype._postOrder = function (node, callback) {
+    if (node !== null) {
+        this.postOrder(node.left, callback);
+        this.postOrder(node.right, callback);
+        callback(node);
+    }
 };
 
 module.exports = BinarySearchTree;
--- unit-tests/binary-search-tree.unittests.js
+++ unit-tests/binary-search-tree.unittests.js
@@ -18,3 +18,106 @@ test('Test empty recursive search', function (assert) {
     assert.equal(actual, false);
     assert.end();
 });
+
+test('Test adding one to empty tree', function (assert) {
+    var bst = new BinarySearchTree();
+
+    bst.insert(5);
+
+    assert.equal(bst.contains(5), true);
+    assert.equal(bst.root.key, 5);
+    assert.end();
+});
+
+test('Test adding ascending keys to tree', function (assert) {
+    var bst = new BinarySearchTree();
+
+    for (var i = 0; i < 10; i++) {
+        bst.insert(i);
+    }
+
+    assert.plan(11);
+
+    var currNode = bst.root;
+    for (var i = 0; i < 10; i++) {
+        assert.equal(currNode.key, i);
+        currNode = currNode.right;
+    }
+
+    assert.equal(currNode, null);
+
+    assert.end();
+});
+
+test('Test adding descending keys to tree', function (assert) {
+    var bst = new BinarySearchTree();
+
+    for (var i = 10; i > 0; i--) {
+        bst.insert(i);
+    }
+
+    assert.plan(11);
+
+    var currNode = bst.root;
+    for (var i = 10; i > 0; i--) {
+        assert.equal(currNode.key, i);
+        currNode = currNode.left;
+    }
+
+    assert.equal(currNode, null);
+
+    assert.end();
+});
+
+test('Test the insertion helper function', function (assert) {
+    var rootNode = new BinarySearchTree.Node(5);
+
+    BinarySearchTree.prototype._insertHelper(rootNode, 3); // rootNode.left
+    BinarySearchTree.prototype._insertHelper(rootNode, 7); // rootNode.right
+    BinarySearchTree.prototype._insertHelper(rootNode, 4); // rootNode.left.right
+    BinarySearchTree.prototype._insertHelper(rootNode, 2); // rootNode.left.left
+
+    assert.equal(rootNode.left.key, 3);
+    assert.equal(rootNode.right.key, 7);
+    assert.equal(rootNode.left.right.key, 4);
+    assert.equal(rootNode.left.left.key, 2);
+
+    assert.end();
+});
+
+test('Test the ordered array conversion', function (assert) {
+    var bst = new BinarySearchTree();
+    var expectedArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    for (var i = 0; i < 10; i++) {
+        bst.insert(i);
+    }
+
+    assert.equal(bst.toInOrderArray().toString(), expectedArray.toString());
+
+    assert.end();
+});
+
+test('Test the ordered array conversion using random values', function (assert) {
+    var bst = new BinarySearchTree();
+    var expectedArray = [];
+    for (var i = 0; i < 10; i++) {
+        var cVal = Math.floor(Math.random() * (99) + 1);
+        expectedArray.push(cVal);
+        bst.insert(cVal);
+    }
+
+    expectedArray = expectedArray.sort(function (a, b) {
+        if (a < b) {
+            return -1;
+        }
+        if (a > b) {
+            return 1;
+        }
+        return 0;
+    });
+
+    assert.equal(bst.toInOrderArray().toString(), expectedArray.toString());
+
+    assert.end();
+});
